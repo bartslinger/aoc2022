b1d1e1d4826542910d9d03914318699b7bb1ce25
Bart Slinger <bartslinger@gmail.com>
2022-12-04 10:17:15 +0100

    day 4 part 2

--- src/04/main.rs
+++ src/04/main.rs
@@ -18,6 +18,12 @@ mod tests {
         let input = parse_input("./src/04/test.txt");
         assert_eq!(count_fully_enclosed(input), 2);
     }
+
+    #[test]
+    fn part_two() {
+        let input = parse_input("./src/04/test.txt");
+        assert_eq!(count_partial_overlap(input), 4);
+    }
 }
 
 type Pair = ((i64, i64), (i64, i64));
@@ -54,10 +60,23 @@ fn count_fully_enclosed(input: Vec<Pair>) -> usize {
     input.iter().filter(|pair| is_enclosed(**pair)).count()
 }
 
+fn overlaps(pair: Pair) -> bool {
+    let first_is_beyond_second = pair.0 .0 > pair.1 .1;
+    let second_is_beyond_first = pair.1 .0 > pair.0 .1;
+    !(first_is_beyond_second || second_is_beyond_first)
+}
+
+fn count_partial_overlap(input: Vec<Pair>) -> usize {
+    input.iter().filter(|pair| overlaps(**pair)).count()
+}
+
 fn main() {
     println!("Hello, day 4!");
 
     let input = parse_input("./input/04/input.txt");
-    let count = count_fully_enclosed(input);
+    let count = count_fully_enclosed(input.clone());
     println!("Part 1: {}", count);
+
+    let count = count_partial_overlap(input);
+    println!("Part 2: {}", count);
 }
