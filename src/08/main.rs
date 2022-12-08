50e754ef1cb249601d01ca442c8085607c0dabad
Bart Slinger <bartslinger@gmail.com>
2022-12-08 20:43:15 +0100

    day 8 part 2

--- src/08/main.rs
+++ src/08/main.rs
@@ -9,24 +9,37 @@ mod tests {
     #[test]
     fn test_visible_sides() {
         let forest = parse_input("./src/08/test.txt");
-        assert_eq!(visible_sides(&forest, [1, 1]), 2);
-        assert_eq!(visible_sides(&forest, [1, 2]), 2);
-        assert_eq!(visible_sides(&forest, [1, 3]), 0);
-        assert_eq!(visible_sides(&forest, [2, 1]), 1);
-        assert_eq!(visible_sides(&forest, [2, 2]), 0);
-        assert_eq!(visible_sides(&forest, [2, 3]), 1);
-        assert_eq!(visible_sides(&forest, [3, 1]), 0);
-        assert_eq!(visible_sides(&forest, [3, 2]), 2);
-        assert_eq!(visible_sides(&forest, [3, 3]), 0);
+        assert_eq!(visible_sides_and_score(&forest, [1, 1]).0, 2);
+        assert_eq!(visible_sides_and_score(&forest, [1, 2]).0, 2);
+        assert_eq!(visible_sides_and_score(&forest, [1, 3]).0, 0);
+        assert_eq!(visible_sides_and_score(&forest, [2, 1]).0, 1);
+        assert_eq!(visible_sides_and_score(&forest, [2, 2]).0, 0);
+        assert_eq!(visible_sides_and_score(&forest, [2, 3]).0, 1);
+        assert_eq!(visible_sides_and_score(&forest, [3, 1]).0, 0);
+        assert_eq!(visible_sides_and_score(&forest, [3, 2]).0, 2);
+        assert_eq!(visible_sides_and_score(&forest, [3, 3]).0, 0);
 
-        assert_eq!(visible_sides(&forest, [0, 0]), 2);
-        assert_eq!(visible_sides(&forest, [4, 3]), 4);
+        assert_eq!(visible_sides_and_score(&forest, [0, 0]).0, 2);
+        assert_eq!(visible_sides_and_score(&forest, [4, 3]).0, 4);
     }
 
     #[test]
     fn test_part_one() {
         let forest = parse_input("./src/08/test.txt");
-        assert_eq!(visible(forest), 21);
+        assert_eq!(visible(&forest), 21);
+    }
+
+    #[test]
+    fn test_scenic_score() {
+        let forest = parse_input("./src/08/test.txt");
+        assert_eq!(visible_sides_and_score(&forest, [1, 2]).1, 4);
+        assert_eq!(visible_sides_and_score(&forest, [3, 2]).1, 8);
+    }
+
+    #[test]
+    fn find_highest_score() {
+        let forest = parse_input("./src/08/test.txt");
+        assert_eq!(highest_score(&forest), 8);
     }
 }
 
@@ -48,47 +61,55 @@ fn parse_input(path: &str) -> ndarray::Array2<u8> {
     forest
 }
 
-fn visible_sides(forest: &ndarray::Array2<u8>, coordinate: [usize; 2]) -> usize {
+fn visible_sides_and_score(forest: &ndarray::Array2<u8>, coordinate: [usize; 2]) -> (usize, usize) {
     let dim = forest.dim();
     let value = forest[coordinate];
     let mut visible_sides = 4;
     // same row to the west
-    for x in 0..coordinate[1] {
+    let mut west = 0;
+    for x in (0..coordinate[1]).rev() {
+        west += 1;
         if forest[[coordinate[0], x]] >= value {
             visible_sides -= 1;
             break;
         }
     }
     // same row to the east
+    let mut east = 0;
     for x in (coordinate[1] + 1)..dim.1 {
+        east += 1;
         if forest[[coordinate[0], x]] >= value {
             visible_sides -= 1;
             break;
         }
     }
     // same column to the north
-    for y in 0..coordinate[0] {
+    let mut north = 0;
+    for y in (0..coordinate[0]).rev() {
+        north += 1;
         if forest[[y, coordinate[1]]] >= value {
             visible_sides -= 1;
             break;
         }
     }
     // same column to the south
+    let mut south = 0;
     for y in (coordinate[0] + 1)..dim.0 {
+        south += 1;
         if forest[[y, coordinate[1]]] >= value {
             visible_sides -= 1;
             break;
         }
     }
-    visible_sides
+    (visible_sides, west * east * north * south)
 }
 
-fn visible(forest: ndarray::Array2<u8>) -> usize {
+fn visible(forest: &ndarray::Array2<u8>) -> usize {
     let (rows, cols) = forest.dim();
     let mut visible_trees = 0;
     for i in 0..cols {
         for j in 0..rows {
-            if visible_sides(&forest, [i, j]) > 0 {
+            if visible_sides_and_score(&forest, [i, j]).0 > 0 {
                 visible_trees += 1;
             }
         }
@@ -96,10 +117,27 @@ fn visible(forest: ndarray::Array2<u8>) -> usize {
     visible_trees
 }
 
+fn highest_score(forest: &ndarray::Array2<u8>) -> usize {
+    let mut highest = 0;
+    let (rows, cols) = forest.dim();
+    for i in 0..cols {
+        for j in 0..rows {
+            let score = visible_sides_and_score(&forest, [i, j]).1;
+            if score > highest {
+                highest = score;
+            }
+        }
+    }
+    highest
+}
+
 fn main() {
     println!("Hello, day 8!");
 
     let input = parse_input("./input/08/input.txt");
-    let visible_trees = visible(input);
+    let visible_trees = visible(&input);
     println!("Part 1: {}", visible_trees);
+
+    let highest_score = highest_score(&input);
+    println!("Part 2: {}", highest_score);
 }
