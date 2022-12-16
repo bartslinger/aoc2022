603f2aa4b0e82acb1c0971f8380a1107a6886690
Bart Slinger <bartslinger@gmail.com>
2022-12-16 16:42:57 +0100

    day 15 part 2

--- src/15/main.rs
+++ src/15/main.rs
@@ -1,5 +1,3 @@
-use std::collections::HashSet;
-
 #[cfg(test)]
 mod tests {
     use super::*;
@@ -8,7 +6,14 @@ mod tests {
     fn test_count_coverage_for_row() {
         let input = parse_input("./src/15/test.txt");
 
-        assert_eq!(coverage_in_row(&input, 10), 26);
+        let coverage = coverage_in_row(&input, 10);
+        assert_eq!(count_coverage_in_row(&coverage), 26);
+    }
+
+    #[test]
+    fn test_find_distress_beacon() {
+        let input = parse_input("./src/15/test.txt");
+        assert_eq!(find_distress_beacon(&input, 20), 56000011);
     }
 }
 
@@ -43,20 +48,20 @@ fn manhatten_distance(a: (i64, i64), b: (i64, i64)) -> i64 {
     dx.abs() + dy.abs()
 }
 
-fn overlaps(a: &(i64, i64), b: &(i64, i64)) -> bool {
-    a.1 >= b.0
+fn overlaps_or_connects(a: &(i64, i64), b: &(i64, i64)) -> bool {
+    a.1 >= (b.0 - 1)
 }
 
 fn merge(a: &(i64, i64), b: &(i64, i64)) -> (i64, i64) {
-    (a.0, b.1)
+    let left = std::cmp::min(a.0, b.0);
+    let right = std::cmp::max(a.1, b.1);
+    (left, right)
 }
 
-fn coverage_in_row(input: &Vec<(Coordinate, Coordinate)>, row: i64) -> i64 {
+fn coverage_in_row(input: &Vec<(Coordinate, Coordinate)>, row: i64) -> Vec<(i64, i64)> {
     let mut coverage = Vec::<(i64, i64)>::new();
-    for pair in input {
-        let sensor = pair.0;
-        let beacon = pair.1;
-        let range = manhatten_distance(sensor, beacon);
+    for (sensor, beacon) in input {
+        let range = manhatten_distance(*sensor, *beacon);
         let dy = row - sensor.1;
         if range >= dy.abs() {
             let remaining = range - dy.abs();
@@ -69,14 +74,31 @@ fn coverage_in_row(input: &Vec<(Coordinate, Coordinate)>, row: i64) -> i64 {
     coverage.sort_by(|a, b| a.0.cmp(&b.0));
     let mut merged_coverage = vec![coverage[0]];
     for range in coverage {
-        if overlaps(&range, merged_coverage.last().unwrap()) {
+        if overlaps_or_connects(merged_coverage.last().unwrap(), &range) {
             // overwrite
             let previous = merged_coverage.pop().unwrap();
             let new = merge(&previous, &range);
             merged_coverage.push(new);
+        } else {
+            merged_coverage.push(range);
         }
     }
-    merged_coverage.iter().map(|(a, b)| b - a).sum()
+    merged_coverage
+}
+
+fn count_coverage_in_row(input: &Vec<(i64, i64)>) -> i64 {
+    input.iter().map(|(a, b)| b - a).sum()
+}
+
+fn find_distress_beacon(input: &Vec<(Coordinate, Coordinate)>, search_limit: i64) -> i64 {
+    for y in 0..search_limit {
+        let coverage = coverage_in_row(input, y);
+        if coverage.len() > 1 {
+            let x = (coverage[0].1 + 1) * 4000000;
+            return x + y;
+        }
+    }
+    0
 }
 
 fn main() {
@@ -84,5 +106,9 @@ fn main() {
 
     let pairs = parse_input("./input/15/input.txt");
     let coverage = coverage_in_row(&pairs, 2000000);
-    println!("Part 1: {}", coverage);
+    let coverage_count = count_coverage_in_row(&coverage);
+    println!("Part 1: {}", coverage_count);
+
+    let tuning_frequency = find_distress_beacon(&pairs, 4000000);
+    println!("Part 2: {}", tuning_frequency);
 }
